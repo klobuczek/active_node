bc43e1bb2d331dcc6c23148cee111c9b992a1c40
Aaron Renner <aaron@animascodelabs.com>
2014-04-03 17:49:08 -0600

    Fix uniqueness validator so records aren't invalid when updating records

--- lib/active_node/validations/uniqueness_validator.rb
+++ lib/active_node/validations/uniqueness_validator.rb
@@ -2,8 +2,18 @@ module ActiveNode
   module Validations
     class UniquenessValidator < ActiveModel::EachValidator # :nodoc:
       def validate_each(record, attribute, value)
-        if value && !record.class.find_by_cypher("Match (n:#{record.class.label}) where n.#{attribute} = {value} return n", value: value).empty?
+        if value && other_matching_records(record, attribute, value).any?
           record.errors.add(attribute, :taken, value: value)
+        end
+      end
+
+      private
+
+      def other_matching_records(record, attribute, value)
+        if record.persisted?
+          record.class.find_by_cypher("Match (n:#{record.class.label}) where n.#{attribute} = {value} and n.id <> {id} return n", value: value, id: record.id)
+        else
+          record.class.find_by_cypher("Match (n:#{record.class.label}) where n.#{attribute} = {value} return n", value: value)
         end
       end
     end
